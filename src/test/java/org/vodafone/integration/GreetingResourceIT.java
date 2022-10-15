4aaa4cc1a1baae07d21d4464c6e25793d4d84440
quarkusio <no-reply@quarkus.io>
2022-10-15 17:07:19 +0000

    Initial commit

---
+++ src/test/java/org/vodafone/integration/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.vodafone.integration;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
